ca8e486b8608436e498f64121f3083bc41459338
kyle <kyle@abc.com>
2017-04-27 10:13:02 +0700

    init

---
+++ app/App.js
@@ -0,0 +1,28 @@
+/**
+ *
+ */
+import React, {Component} from 'react';
+import {
+    StyleSheet,
+    View,
+    Text
+} from 'react-native';
+
+class App extends Component {
+    render() {
+        return (
+            <View
+                style={styles.container
+                }>
+                <Text> App </ Text >
+            </View >
+        );
+    }
+}
+const styles = StyleSheet.create({
+    container: {
+        flex: 1
+    }
+});
+
+export default App;
