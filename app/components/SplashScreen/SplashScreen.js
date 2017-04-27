e8def8460106a700bdfc05ad0e54b937c4ad29d4
kyle <kyle@abc.com>
2017-04-27 14:32:13 +0700

    init

--- app/components/SplashScreen/SplashScreen.js
+++ app/components/SplashScreen/SplashScreen.js
@@ -5,22 +5,48 @@ import React, {Component} from 'react';
 import {
     StyleSheet,
     View,
-    Text
+    ActivityIndicator,
+    Text,
+    Image
 } from 'react-native';
 
 class SplashScreen extends Component {
     render() {
         return (
             <View style={styles.container}>
-                <Text>SplashScreen</Text>
+                <Image source={ require('../../assets/img/ic_laucher.png') } style={styles.imgLaunch}/>
+                <ActivityIndicator
+                    animating
+                    color="#eeeeee"
+                    style={[styles.centering, styles.gray]}
+                    size="small"
+                />
             </View>
         );
     }
 }
 const styles = StyleSheet.create({
-    container: {
-        flex: 1
-    }
-});
+        container: {
+            flex: 1,
+            alignItems: 'center',
+            justifyContent: 'center',
+            backgroundColor: '#07888e',
+            flexDirection: 'column'
+        },
+        imgLaunch: {
+            width: 80,
+            height: 80,
+            marginBottom: 5
+        },
+        title: {
+            color: '#ffffff'
+        },
+        centering: {
+            alignItems: 'center',
+            justifyContent: 'center',
+            padding: 8,
+        }
+    })
+;
 
 export default SplashScreen;
